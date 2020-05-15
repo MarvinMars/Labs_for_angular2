8d8ec3d1ffe1fa03d750dab96b271b8626dc2215
Bogdan Shuplyak <bogdan.shuplyak@gmail.com>
2020-05-15 22:56:58 +0300

    lab 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MyValidator} from "./my.validator";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app2';
+
+export class AppComponent implements OnInit{
+  form: FormGroup;
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        MyValidator.restrictedEmails
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(10)
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('Запорожье', [
+          Validators.required,
+          Validators.minLength(3)
+        ])
+      }),
+      skills: new FormArray([])
+    })
+  }
+
+  setCapital() {
+    const mapCity = {
+      ua: "Киев",
+      pl: "Варшава",
+      de: "Берлин"
+    };
+    const getCity = this.form.get('address').value.country;
+    this.form.get('address').patchValue({city: mapCity[getCity]});
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    (<FormArray>this.form.get('skills')).push(control);
+  }
+
+  removeSkill(index) {
+    (<FormArray>this.form.get('skills')).removeAt(index)
+  }
+
+  submit() {
+    const formControl = {...this.form.value};
+    console.log(formControl);
+    this.form.reset();
+  }
 }
